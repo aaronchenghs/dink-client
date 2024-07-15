46276c79b178de5f119a45793b7a5bd68df814f4
Aaron Cheng <acheng8@lsu.edu>
2024-07-14 21:43:35 -0500

    fixed build errors

--- src/components/courtmap/googlemapsloader.tsx
+++ src/components/courtmap/googlemapsloader.tsx
@@ -1,11 +1,14 @@
-import { LoadScript } from "@react-google-maps/api";
+import { Libraries, LoadScript } from "@react-google-maps/api";
 
 const MAPS_API_KEY = import.meta.env.VITE_MAPS_API_KEY as string;
 const libraries = ["places"];
 
 const GoogleMapsLoader = ({ children }: { children: React.ReactNode }) => {
   return (
-    <LoadScript googleMapsApiKey={MAPS_API_KEY} libraries={libraries}>
+    <LoadScript
+      googleMapsApiKey={MAPS_API_KEY}
+      libraries={libraries as Libraries}
+    >
       {children}
     </LoadScript>
   );
